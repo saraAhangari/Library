411c68a52c3290a6c2288b4ce1def1633c5f1941
saraAhangari <sara.ahn02@gmail.com>
2021-12-18 12:56:28 +0330

    post new author added

--- WebApplication2/Services/sqlAuthorData.cs
+++ WebApplication2/Services/sqlAuthorData.cs
@@ -31,5 +31,12 @@ namespace WebApplication2.Utils
         {
             return _authorContext.Books.Where(b => b.author == authorName).ToList();
         }
+
+        public Author AddAuthor(Author author)
+        {
+            _authorContext.Authors.Add(author);
+            _authorContext.SaveChanges();
+            return author;
+        }
     }
 }
