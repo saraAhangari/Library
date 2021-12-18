cf88143aef2cd6eea00b9acbdbab1383222cf96c
saraAhangari <sara.ahn02@gmail.com>
2021-12-18 11:52:55 +0330

    book category updated

---
+++ WebApplication2/Services/sqlCategoryData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebApplication2.Models;
+
+namespace WebApplication2.Services
+{
+    public class sqlCategoryData
+    {
+        private BookContext _categoryContext;
+
+        public sqlCategoryData(BookContext _categoryContext)
+        {
+            this._categoryContext = _categoryContext;
+        }
+
+        public List<BookCategory> GetCategories()
+        {
+            return _categoryContext.BookCategory.ToList();
+        }
+
+        public BookCategory AddBook(BookCategory category)
+        {
+            _categoryContext.BookCategory.Add(category);
+            _categoryContext.SaveChanges();
+            return category;
+        }
+    }
+}
---
+++ WebApplication2/Utils/ICategoryData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using WebApplication2.Models;
+
+namespace WebApplication2.Utils
+{
+    public interface ICategoryData
+    {
+        List<BookCategory> GetCategories();
+        BookCategory AddCategory(BookCategory category);
+
+    }
+}
