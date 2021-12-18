edf6b35aa9a8e8cc7832c2d00ea6ebb75935f698
saraAhangari <sara.ahn02@gmail.com>
2021-12-18 10:21:20 +0330

    add tables

--- WebApplication2/Models/BookContext.cs
+++ WebApplication2/Models/BookContext.cs
@@ -8,5 +8,10 @@ namespace WebApplication2.Models
         {
         }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+
+        public DbSet<AuthorContact> AuthorContact { get; set; }
+        public DbSet<BookCategory> BookCategory { get; set; }
     }
 }
